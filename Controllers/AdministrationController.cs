a7ab327af8e0c19d5c1656291290dbd13703c030
Shantilal Suthar <sls.14581@gmail.com>
2026-07-19 19:18:17 +0530

    19-07-2026 : Delete role identity.

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -211,6 +211,31 @@ namespace EmployeeManagement.Controllers
             return RedirectToAction("EditRole", new { Id = roleId });
         }
 
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var result = await _roleManager.DeleteAsync(role);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("ListRoles");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View("ListRoles");
+        }
         [HttpGet]
         public IActionResult ListUsers()
         {
